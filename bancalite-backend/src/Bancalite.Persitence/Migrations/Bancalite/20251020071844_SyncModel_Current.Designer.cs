89062f9c6e74368bd5f1a2cf4190415064e29d13
Alexis Dorado Muñoz <alexis.dorado.munoz@gmail.com>
2025-10-20 03:02:41 -0500

    # SET-04 | T-S2.3 feat(build): configura migraciones automáticas y añade índices únicos  `SET-04 | T-S2.3` Feat(build): configura migraciones automáticas y añade índices únicos
    * Se refactoriza y documenta el `Dockerfile` para optimizar el cacheo de capas y utilizar la imagen SDK, permitiendo la ejecución de migraciones automáticas al inicio.
    * Se agregan índices únicos a las columnas `AppUserId` y `Email` en la base de datos para garantizar la integridad de los datos.
    * Referencias: [US_ID=SET-04] [Task_ID=T-S2.3]
    
    **Tarea:** #123

--- bancalite-backend/src/Bancalite.Persitence/Migrations/Bancalite/20251020071844_SyncModel_Current.Designer.cs
+++ bancalite-backend/src/Bancalite.Persitence/Migrations/Bancalite/20251020071844_SyncModel_Current.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Bancalite.Persitence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Bancalite.Persitence.Migrations.Bancalite
 {
     [DbContext(typeof(BancaliteContext))]
-    partial class BancaliteContextModelSnapshot : ModelSnapshot
+    [Migration("20251020071844_SyncModel_Current")]
+    partial class SyncModel_Current
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
